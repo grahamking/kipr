a3edbd47515a0c103cbcad5280acd60363517aa0
Graham King <graham@gkgk.org>
2020-10-18 16:22:12 -0700

    show: Display result, copy to clipboard.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ const HOME_PWD: &str = "/home/graham/.kip/passwords";
 // DECRYPT_CMD = config.get('gnupg', 'decrypt_cmd')
 const DECRYPT_CMD: &str = "gpg --quiet --decrypt";
 
+//if sys.platform == 'darwin':
+//    CLIP_CMD = 'pbcopy'
+//else:
+const CLIP_CMD: &str = "xclip";
+
 fn main() {
     let args_vec = env::args().collect();
     let args = parse_args(args_vec);
@@ -40,7 +45,7 @@ fn parse_args(args: Vec<String>) -> Args {
         cmd: String::from("get"),
         username: String::from(""),
         filepart: String::from(&args[1]),
-        is_print: true,
+        is_print: false,
     };
 }
 
@@ -61,34 +66,23 @@ impl Args {
 
 // Display accounts details for name, and put password on clipboard
 fn show(name: &str, is_visible: bool) -> anyhow::Result<()> {
-    println!("show: {} {}", name, is_visible);
-
     let entry = match find(name) {
         Some(filename) => {
             println!("found {}", filename.to_str().unwrap());
-            extract(filename)
+            extract(filename)?
         }
         None => {
             return Err(anyhow::anyhow!("File not found: {}", name));
         }
     };
+    println!("{}", bold(&entry.username));
+    if is_visible {
+        println!("{}", entry.password);
+    } else {
+        copy_to_clipboard(&entry.password)?;
+    }
+    println!("{}", entry.notes);
 
-    println!("{:?}", entry);
-
-    // TODO: work here
-
-    /*
-    print(bold(username))
-
-    if is_visible:
-        print(password)
-    else:
-        copy_to_clipboard(password)
-
-    print(notes)
-
-    return 0
-    */
     Ok(())
 }
 
@@ -98,6 +92,9 @@ fn find(name: &str) -> Option<path::PathBuf> {
     let filename = path::Path::new(HOME_PWD).join(name);
     if let Err(_) = filename.metadata() {
         println!("dunno");
+
+        // TODO: work here
+
         //filename = guess(name)
         //basename = os.path.basename(filename)
         //print('Guessing {}'.format(bold(basename)))
@@ -131,15 +128,24 @@ fn extract(filename: path::PathBuf) -> anyhow::Result<Entry> {
 }
 
 fn decrypt(contents: &str) -> anyhow::Result<String> {
-    execute(DECRYPT_CMD, Some(contents))
+    execute(DECRYPT_CMD, Some(contents), true)
+}
+
+// Copy given message to clipboard
+fn copy_to_clipboard(msg: &str) -> anyhow::Result<String> {
+    execute(CLIP_CMD, Some(msg), false)
 }
 
 // Run 'cmd' in sub-process on 'data_in' and return output.
-fn execute(cmd: &str, data_in: Option<&str>) -> anyhow::Result<String> {
+fn execute(cmd: &str, data_in: Option<&str>, has_out: bool) -> anyhow::Result<String> {
     let mut parts_iter = cmd.split(' ');
     let bin = parts_iter.next().unwrap();
     let mut proc = process::Command::new(bin);
-    proc.stdout(process::Stdio::piped());
+    if has_out {
+        proc.stdout(process::Stdio::piped());
+    } else {
+        proc.stdout(process::Stdio::null());
+    }
     proc.stderr(process::Stdio::null()); // comment out for debugging
     for a in parts_iter {
         proc.arg(a);
@@ -152,7 +158,16 @@ fn execute(cmd: &str, data_in: Option<&str>) -> anyhow::Result<String> {
         let stdin = child.stdin.as_mut().unwrap();
         stdin.write_all(data_in.unwrap().as_bytes())?;
     }
+    if !has_out {
+        child.wait()?;
+        return Ok(String::new());
+    }
     let out = child.wait_with_output()?;
     let out_str = String::from_utf8(out.stdout)?;
     Ok(out_str)
+}
+
+// 'msg' wrapped in ANSI escape sequence to make it bold
+fn bold(msg: &str) -> String {
+    format!("\x1b[1m{}\x1b[0m", msg)
 }
